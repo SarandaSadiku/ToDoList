186d62aa6d3a728c26472f69cb6e93383e073841
Saranda <sarandasadiku2@gmail.com>
2019-12-13 09:27:56 +0100

    create react app

---
+++ src/components/Todos.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+
+class Todos extends Component {
+ render() {
+   return (
+    <div>
+      <h1>Todos</h1>
+    </div>
+   );
+ }
+}
+
+export default Todos;
