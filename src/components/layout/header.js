1208bd34290b6cbeee12cc617d4a13899d8a7d45
Saranda <sarandasadiku2@gmail.com>
2019-12-17 14:42:38 +0100

    created about page

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <header style={headerStyle}>
             <h1>TodoList</h1>
+            <Link style={LinkStyle} to="/">Home</Link> | <Link style={LinkStyle} to="/about">About</Link>
         </header>
     )
 }
@@ -12,6 +14,11 @@ const headerStyle = {
     color: '#fff',
     textAlign: 'center',
     padding: '10px'
+}
+
+const LinkStyle = {
+    color: '#fff',
+    textDecoration: 'none'
 }
 
 export default Header;
